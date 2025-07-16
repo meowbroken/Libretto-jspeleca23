c561b96ff0bd31351d4d8d3624a73bc3bc4a2e34
Philip James Olbedencia <meowbroken@gmail.com>
2025-07-16 15:37:01 +0800

    Adding navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,18 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/authors') }}">Authors</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/books') }}">Books</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/genres') }}">Genres</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/reviews') }}">Reviews</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
